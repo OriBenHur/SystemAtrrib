63c64c8df3555f26b9842f13abe320afb4d58685
Ori <oribenhur@gmail.com>
2017-01-04 01:11:19 +0200

    Initial commit

---
+++ SystemAtrrib/SystemAtrrib.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace SystemAtrrib
+{
+    partial class SystemAtrrib
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SystemAtrrib));
+            this.SysFile = new System.Windows.Forms.Button();
+            this.SysFolder = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // SysFile
+            // 
+            this.SysFile.BackColor = System.Drawing.Color.Red;
+            this.SysFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.SysFile.Location = new System.Drawing.Point(28, 10);
+            this.SysFile.Name = "SysFile";
+            this.SysFile.Size = new System.Drawing.Size(180, 40);
+            this.SysFile.TabIndex = 0;
+            this.SysFile.Text = "הפוך לקובץ מערכת";
+            this.SysFile.UseVisualStyleBackColor = false;
+            this.SysFile.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // SysFolder
+            // 
+            this.SysFolder.BackColor = System.Drawing.Color.Red;
+            this.SysFolder.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.SysFolder.Location = new System.Drawing.Point(218, 10);
+            this.SysFolder.Name = "SysFolder";
+            this.SysFolder.Size = new System.Drawing.Size(180, 40);
+            this.SysFolder.TabIndex = 1;
+            this.SysFolder.Text = "הפוך לתיקית מערכת";
+            this.SysFolder.UseVisualStyleBackColor = false;
+            this.SysFolder.Click += new System.EventHandler(this.SysFolder_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Lime;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.button1.Location = new System.Drawing.Point(28, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 40);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "ביטול הגדרת קובץ מערכת";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Lime;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.button3.Location = new System.Drawing.Point(218, 56);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(180, 40);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "ביטול הגדרת תיקית מערכת";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // SystemAtrrib
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(428, 114);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.SysFolder);
+            this.Controls.Add(this.SysFile);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "SystemAtrrib";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.RightToLeftLayout = true;
+            this.Text = "מה תרצה לעשות?";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.SystemAtrrib_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SysFile;
+        private System.Windows.Forms.Button SysFolder;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ SystemAtrrib/SystemAtrrib.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+
+namespace SystemAtrrib
+{
+    public partial class SystemAtrrib : Form
+    {
+        public SystemAtrrib()
+        {
+            InitializeComponent();
+        }
+
+        public void Hidden(string Hidden, int num,string ShowSuperHidden,int num2)
+        {
+            RegistryKey key = Registry.CurrentUser.CreateSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced");
+            key.SetValue(Hidden, num);
+            key.SetValue(ShowSuperHidden, num2);
+            SendKeys.Send("{F5}");
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //IFileDialog fg = new IFileDialog();
+            OpenFileDialog file = new OpenFileDialog();
+            file.Multiselect = true;
+            SendKeys.Send("^ ");
+            Hidden("Hidden", 2, "ShowSuperHidden", 0);
+            
+            DialogResult result = file.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                foreach (string ver in file.FileNames)
+                {
+                    FileAttributes attributes = File.GetAttributes(ver);
+                    File.SetAttributes(ver, File.GetAttributes(ver) | FileAttributes.Hidden);
+                    File.SetAttributes(ver, File.GetAttributes(ver) | FileAttributes.System);
+                    MessageBox.Show(Path.GetFileName(ver) + " is now hidden");
+                }
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            
+
+            OpenFileDialog file = new OpenFileDialog();
+            file.Multiselect = true;
+            SendKeys.Send("^ ");
+            Hidden("Hidden", 1, "ShowSuperHidden", 1);
+            DialogResult result = file.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+
+                foreach (string ver in file.FileNames)
+                {
+                    FileAttributes attributes = File.GetAttributes(ver);
+                    File.SetAttributes(ver, FileAttributes.Normal);
+                    MessageBox.Show(Path.GetFileName(ver) + " is now a normal file");
+                }
+                SendKeys.Send("^ ");
+                Hidden("Hidden", 2, "ShowSuperHidden", 0);
+                
+            }
+            else
+            {
+                SendKeys.Send("^ ");
+                Hidden("Hidden", 2, "ShowSuperHidden", 0);
+                
+            }
+        }
+
+        private void SystemAtrrib_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SendKeys.Send("^ ");
+            Hidden("Hidden", 2, "ShowSuperHidden", 0);
+            
+        }
+
+        private void SysFolder_Click(object sender, EventArgs e)
+        {
+            SendKeys.Send("^ ");
+            Hidden("Hidden", 2, "ShowSuperHidden", 0);
+            FolderSelectDialog folder = new FolderSelectDialog();
+            folder.InitialDirectory = "C:\\";
+            bool result = folder.ShowDialog();
+            if (result)
+            {
+                MessageBox.Show(Path.GetFileName(folder.FileName) + " is now hidden");
+                FileAttributes attributes = File.GetAttributes(folder.FileName);
+                File.SetAttributes(folder.FileName, File.GetAttributes(folder.FileName) | FileAttributes.Hidden);
+                File.SetAttributes(folder.FileName, File.GetAttributes(folder.FileName) | FileAttributes.System);
+                
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SendKeys.Send("^ ");
+            Hidden("Hidden", 1, "ShowSuperHidden", 1);
+            
+            FolderSelectDialog folder = new FolderSelectDialog();
+            folder.InitialDirectory = "C:\\";
+            bool result = folder.ShowDialog();
+            if (result)
+            {
+                MessageBox.Show(Path.GetFileName(folder.FileName) + " is now normal");
+                FileAttributes attributes = File.GetAttributes(folder.FileName);
+                File.SetAttributes(folder.FileName, FileAttributes.Normal);
+                SendKeys.Send("^ ");
+                Hidden("Hidden", 2, "ShowSuperHidden", 0);
+
+            }
+            else
+            {
+                SendKeys.Send("^ ");
+                Hidden("Hidden", 2, "ShowSuperHidden", 0);
+                
+            }
+        }
+    }
+}
